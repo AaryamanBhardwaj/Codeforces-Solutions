7849318eb99dd2cc4a08ab66f715c8b7905a8be3
AaryamanBhardwaj <74190514+AaryamanBhardwaj@users.noreply.github.com>
2021-05-31 17:05:13 +0530

    Add files via upload

---
+++ fillingdiamonds.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t,n;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>n;
+        cout<<n<<endl;
+    }
+}
