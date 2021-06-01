d33c75bfa493b16a3c1dfc67e52b578a40872199
AaryamanBhardwaj <74190514+AaryamanBhardwaj@users.noreply.github.com>
2021-06-01 19:19:31 +0530

    Add files via upload

---
+++ wizardsduel.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int l,p,q;
+    cin>>l>>p>>q;
+    cout<<(float)(l*p)/(p+q);
+}
